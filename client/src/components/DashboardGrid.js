5f40af81769a91248442f9488498407c5553eb5f
CalebMKu <calebmkutz@gmail.com>
2022-04-13 15:32:47 -0400

    Third Commit

--- client/src/components/DashboardGrid.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Grid } from "@chakra-ui/react";
-
-const DashboardGrid = ({ children }) => {
-  return <Grid templateColumns="repeat(2, 1fr)" gap={6}>{children}</Grid>;
-};
-
-export default DashboardGrid;
